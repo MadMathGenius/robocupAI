24eec0e16dc14af5bad93b6b142e7810b5ac08a2
MadMathGenius <daksman2019@gmail.com>
2025-10-31 16:11:33 +0200

    file edits

--- start1.sh
+++ start1.sh
@@ -5,5 +5,5 @@ host=${1:-localhost}
 port=${2:-3100}
 
 for i in {1..5}; do
-  python3 ./Run_Player.py -i $host -p $port -u $i -t 'diego' -P 0 -D 0 &
+  python3 ./Run_Player.py -i $host -p $port -u $i -t 'Daksman' -P 0 -D 0 &
 done
